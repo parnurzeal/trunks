eb2ca2e9b3ef8765c8c4f744c5848286252047f8
Theeraphol Wattanavekin <theeraphol.wat@gmail.com>
2014-10-31 21:22:42 +0900

    Added aws controller, and aws-sdk module, shrinkwrap all versions

---
+++ api/controllers/AwsController.js
@@ -0,0 +1,19 @@
+/**
+ * AwsController
+ *
+ * @description :: Server-side logic for managing aws
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+ /*jshint node: true */
+ /*global sails  */
+ 'use strict';
+
+module.exports = {
+	'get_ec2': function(req,res,next){
+
+
+		
+		res.json(200,"HELLO");
+	}
+};
+
