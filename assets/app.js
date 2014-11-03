1b55667499c6b66ca5dd0cefd320ba5741682f24
Rapee Suveeranont <iiieyes@gmail.com>
2014-11-03 12:12:10 +0800

    Added home view

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.view3',
+  'myApp.home'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
